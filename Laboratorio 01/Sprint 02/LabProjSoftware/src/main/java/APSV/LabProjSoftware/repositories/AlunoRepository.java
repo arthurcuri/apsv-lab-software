984cfd8cffa034aa30bf6b924b141795a67e45f1
Vinicius Xavier <101216409+ViniciusXR@users.noreply.github.com>
2025-02-26 21:54:09 -0300

    Delete Laboratorio 01/Sprint 02/LabProjSoftware directory

--- Laboratorio 01/Sprint 02/LabProjSoftware/src/main/java/APSV/LabProjSoftware/repositories/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package APSV.LabProjSoftware.repositories;
-
-import APSV.LabProjSoftware.models.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
